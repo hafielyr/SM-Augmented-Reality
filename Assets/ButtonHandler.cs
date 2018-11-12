62013563f992dd7b7a5ac1d5d9ae3c24dc04f76a
unknown <hafieluyr@gmail.com>
2018-11-12 21:43:39 +0800

    first commit

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour {
+
+	public GameObject text;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Button1_onclick(){
+		Debug.Log ("b1 clicked");
+		text.GetComponent<UnityEngine.UI.Text  > ().text  = "TEST 1";
+	}
+
+	public void Button2_onclick(){
+		Debug.Log ("b2 clicked");
+		text.GetComponent<UnityEngine.UI.Text  > ().text  = "TEST 2";
+	}
+}
